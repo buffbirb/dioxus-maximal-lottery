e54eee182d4dc81aa5e4c96a09de89550ed5056c
octoclonius <25781800+octoclonius@users.noreply.github.com>
2026-07-24 23:59:16 -0500

    feat: Render deployment pipeline via Nix-built containers and Supabase migrations

---
+++ image.nix
@@ -0,0 +1,47 @@
+{
+  artifact ? ./target/dx/web/release/web,
+  nixpkgs ?
+    let
+      locked = (builtins.fromJSON (builtins.readFile ./devenv.lock)).nodes.nixpkgs.locked;
+    in
+    builtins.fetchTarball {
+      sha256 = locked.narHash;
+      url = "https://github.com/${locked.owner}/${locked.repo}/archive/${locked.rev}.tar.gz";
+    },
+  pkgs ? import nixpkgs { system = builtins.currentSystem; },
+  tag ? "latest",
+}:
+let
+  server = pkgs.stdenv.mkDerivation {
+    buildInputs = with pkgs; [
+      stdenv.cc.cc.lib
+    ];
+    dontUnpack = true;
+    installPhase = ''
+      runHook preInstall
+      mkdir -p "${out}/app"
+      cp -r "${src}"/. "${out}/app/"
+      chmod +x "${out}/app/web"
+      runHook postInstall
+    '';
+    nativeBuildInputs = with pkgs; [
+      autoPatchelfHook
+    ];
+    pname = "maximal-lottery-web";
+    src = artifact;
+    version = "0.1.0";
+  };
+in
+pkgs.dockerTools.buildLayeredImage {
+  config = {
+    Cmd = [
+      "/app/web"
+    ];
+    WorkingDir = "/app";
+  };
+  contents = [
+    server
+  ];
+  name = "maximal-lottery-web";
+  inherit tag;
+}
