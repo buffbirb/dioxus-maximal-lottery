7ac9fc2278189a9ddeb2cd0b13226e11f1573341
octoclonius <25781800+octoclonius@users.noreply.github.com>
2026-07-26 08:01:43 -0500

    lint image.nix

--- image.nix
+++ image.nix
@@ -12,29 +12,32 @@
   tag ? "latest",
 }:
 let
-  server = pkgs.stdenv.mkDerivation (
-    {
-      dontUnpack = true;
-      installPhase = ''
-        runHook preInstall
-        mkdir -p "$out/app"
-        cp -r "$src"/. "$out/app/"
-        chmod +x "$out/app/server"
-        runHook postInstall
-      '';
-      pname = "maximal-lottery-web";
-      src = artifact;
-      version = "0.1.0";
-    }
-    // pkgs.lib.optionalAttrs pkgs.stdenv.isLinux {
-      buildInputs = with pkgs; [
+  server = pkgs.stdenv.mkDerivation {
+    buildInputs = pkgs.lib.optionals pkgs.stdenv.isLinux (
+      with pkgs;
+      [
         stdenv.cc.cc.lib
-      ];
-      nativeBuildInputs = with pkgs; [
+      ]
+    );
+    dontUnpack = true;
+    installPhase = ''
+      runHook preInstall
+      mkdir -p "$out/app"
+      cp -r "$src"/. "$out/app/"
+      chmod +x "$out/app/server"
+      runHook postInstall
+    '';
+    # Linux-only inputs so the Nix expression can evaluate on macOS.
+    nativeBuildInputs = pkgs.lib.optionals pkgs.stdenv.isLinux (
+      with pkgs;
+      [
         autoPatchelfHook
-      ];
-    }
-  );
+      ]
+    );
+    pname = "maximal-lottery-web";
+    src = artifact;
+    version = "0.1.0";
+  };
 in
 pkgs.dockerTools.buildLayeredImage {
   config = {
